eb27019ccc0545420243070b802ec0dc2c741f5d
Ashleigh Maund <ashleigh.maund@bcsg.com>
2016-08-02 16:12:15 +0100

    able to add multiple items at a time to order, show correct total, also hiding and showing correct buttons when we take order and take payment

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 // Want to pass the Menu's state.menu to parent Cafe as a property, if possible.
-// For now, menu is inside the till, but will transfer eventually.
 // Am now keeping the menu in Cafe and passing to Menu. Will amend soon.
 
 
@@ -86,6 +85,8 @@ var Till = React.createClass({
 		return {
 			orderItems: [],
 			orderTotal: 0,
+			takingPayment: false,
+			itemQuantity: '',
 			customerPayment: '',
 			changeDueToCustomer: 0
 		}
@@ -102,28 +103,69 @@ var Till = React.createClass({
 		this.uniqueId = this.uniqueId || 0;
 		return this.uniqueId++;
 	},
-	takeCustomerPayment: function(element) {
-		if (!((this.state.customerPayment.indexOf('.') !== -1) && (this.state.customerPayment.length - 1) - this.state.customerPayment.indexOf('.') >= 2)) {
-			this.setState({customerPayment: this.state.customerPayment + element.target.value});
-		}
+
+
+
+	enterValue: function(element) {
+		if (this.state.takingPayment == true) {
+			if (!((this.state.customerPayment.indexOf('.') !== -1) && (this.state.customerPayment.length - 1) - this.state.customerPayment.indexOf('.') >= 2)) {
+				this.setState({customerPayment: this.state.customerPayment + element.target.value});
+			};
+		} else {
+			if (element.target.value != '.' && this.state.itemQuantity.length < 2) {
+				this.setState({itemQuantity: this.state.itemQuantity + element.target.value});
+			};
+		};
 	},
+
+
+
+	takePayment: function() {
+		this.state.itemQuantity = '';
+		this.state.takingPayment = true;
+	},
+
+
+
 	deleteLastDigit: function() {
-		newCustPayment = this.state.customerPayment.substr(0, this.state.customerPayment.length - 1)
-		this.setState({customerPayment: newCustPayment});
+		if (this.state.takingPayment == true) {
+			var newCustPayment = this.state.customerPayment.substr(0, this.state.customerPayment.length - 1);
+			this.setState({customerPayment: newCustPayment});
+		} else {
+			var amendedItemQuant = this.state.itemQuantity.substr(0, this.state.itemQuantity.length - 1);
+			this.setState({itemQuantity: amendedItemQuant});
+		};
 	},
+
+
+
 	calcChangeDueToCustomer: function() {
 		if (this.state.customerPayment - this.state.orderTotal >= 0) {
 			this.setState({changeDueToCustomer: this.state.customerPayment - this.state.orderTotal});
 		};
 	},
+
+
+
 	addToOrder: function() {
+		console.log(this.state.itemQuantity)
+		var quantityOfItem = this.state.itemQuantity;
+		if (quantityOfItem == '') {
+			quantityOfItem = 1;
+		}
 		var allItemsOnOrder = this.state.orderItems;
 		var itemToAdd = ReactDOM.findDOMNode(this.refs.selectedMenuItem).value;
-		var itemArray = {item: itemToAdd, id: this.nextId()};
+		var itemArray = {item: itemToAdd, quantity: quantityOfItem, id: this.nextId()};
 		allItemsOnOrder.push(itemArray);
-		this.setState({orderItems: allItemsOnOrder});
-		this.setState({orderTotal: this.state.orderTotal + this.props.cafeMenu[itemToAdd]});
+		console.log('mooo', this.props.cafeMenu[itemToAdd] * quantityOfItem)
+		this.setState({orderItems: allItemsOnOrder,
+			orderTotal: this.state.orderTotal + (this.props.cafeMenu[itemToAdd] * quantityOfItem),
+			itemQuantity: ''}
+		);
 	},
+
+
+
 	render: function() {
 
 
@@ -138,39 +180,48 @@ var Till = React.createClass({
 			<div
 				className="till-container">
 				<div className="calculator">
-					<p>Choose an item to add to order:</p>
-					<select
-						ref="selectedMenuItem">
-						{Object.keys(menu).map(this.eachMenuItem)}
-					</select>
-					<button
-						className="allCalcBtns otherBtns"
-						onClick={this.addToOrder}>
-						Add to Order
-					</button>
-
+					<div className="item-quantity otherBtns">
+						{this.state.itemQuantity} {this.state.customerPayment}
+					</div>
+					<div className={this.state.takingPayment ? 'hidden till-top-row' : 'till-top-row'}>
+						<p>Choose an item to add to order:</p>
+						<select
+							ref="selectedMenuItem">
+							{Object.keys(menu).map(this.eachMenuItem)}
+						</select>
+						<button
+							className="allCalcBtns otherBtns"
+							onClick={this.addToOrder}>
+							Add to Order
+						</button>
+					</div>
 					<div>
 						<div>
-							<input className="allCalcBtns numberBtns" type="button" onClick={this.takeCustomerPayment} value="1" />
-							<input className="allCalcBtns numberBtns" type="button" onClick={this.takeCustomerPayment} value="2" />
-							<input className="allCalcBtns numberBtns" type="button" onClick={this.takeCustomerPayment} value="3" />
+							<input className="allCalcBtns numberBtns" type="button" onClick={this.enterValue} value="1" />
+							<input className="allCalcBtns numberBtns" type="button" onClick={this.enterValue} value="2" />
+							<input className="allCalcBtns numberBtns" type="button" onClick={this.enterValue} value="3" />
 						</div>
 						<div>
-							<input className="allCalcBtns numberBtns" type="button" onClick={this.takeCustomerPayment} value="4" />
-							<input className="allCalcBtns numberBtns" type="button" onClick={this.takeCustomerPayment} value="5" />
-							<input className="allCalcBtns numberBtns" type="button" onClick={this.takeCustomerPayment} value="6" />
+							<input className="allCalcBtns numberBtns" type="button" onClick={this.enterValue} value="4" />
+							<input className="allCalcBtns numberBtns" type="button" onClick={this.enterValue} value="5" />
+							<input className="allCalcBtns numberBtns" type="button" onClick={this.enterValue} value="6" />
 						</div>
 						<div>
-							<input className="allCalcBtns numberBtns" type="button" onClick={this.takeCustomerPayment} value="7" />
-							<input className="allCalcBtns numberBtns" type="button" onClick={this.takeCustomerPayment} value="8" />
-							<input className="allCalcBtns numberBtns" type="button" onClick={this.takeCustomerPayment} value="9" />
+							<input className="allCalcBtns numberBtns" type="button" onClick={this.enterValue} value="7" />
+							<input className="allCalcBtns numberBtns" type="button" onClick={this.enterValue} value="8" />
+							<input className="allCalcBtns numberBtns" type="button" onClick={this.enterValue} value="9" />
 						</div>
 						<div>
-							<input className="allCalcBtns numberBtns" type="button" onClick={this.takeCustomerPayment} value="0" />
-							<input className="allCalcBtns numberBtns" type="button" onClick={this.takeCustomerPayment} value="." />
+							<input className="allCalcBtns numberBtns" type="button" onClick={this.enterValue} value="0" />
+							<input className="allCalcBtns numberBtns" type="button" onClick={this.enterValue} value="." />
 						</div>
+						<input className={this.state.takingPayment ? 'hidden allCalcBtns otherBtns' : 'allCalcBtns otherBtns' } type="button" onClick={this.takePayment} value="Order finished" />
 						<input className="allCalcBtns otherBtns" type="button" onClick={this.deleteLastDigit} value="Delete" />
-						<input className="allCalcBtns otherBtns" type="button" onClick={this.calcChangeDueToCustomer} value="Calculate change due" />
+						<input className={this.state.takingPayment ? 'allCalcBtns otherBtns' : 'hidden allCalcBtns otherBtns' } type="button" onClick={this.calcChangeDueToCustomer} value="Calculate change due" />
+
+
+
+
 					</div>
 				</div>
 				<Receipt
@@ -200,6 +251,9 @@ var Receipt = React.createClass({
 				id={i}>
 				<div className="receipt-item">
 					{orderItem.item}
+				</div>
+				<div className="receipt-item-quantity">
+					x {orderItem.quantity}
 				</div>
 				<div className="receipt-item-price">
 					£{this.props.cafeMenu[orderItem.item]}
